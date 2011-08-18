162ab8ed710c9ff4be7691441af22f2876270bc1
animon <animon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-18 11:46:00 +0000

    First commit of Easy AdSense Pro
    git-svn-id: https://plugins.svn.wordpress.org/easy-adsense-pro/trunk@425334 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ myPlugins.php
@@ -0,0 +1,53 @@
+<?php
+
+$myPlugins = array() ;
+
+$myPlugins['easy-adsense'] =
+  array('value' => 'Easy AdSense',
+    'support' => 'HYZ5AWPYSC8VA',
+    'price' => '4.95' ,
+    'desc' => 'manages all aspects of Google AdSense for your blog. Easy and complete!',
+    'pro' => 'The Lite version of Easy AdSense is fully functional. But it has an ad slot sharing mechanism built in whereby you can donate about 5% of your ad spaces to the author of the plugin. It also puts a small powered-by kind of link below the first ad block on your pages. The Pro version has these options turned off, and gives you options on the admin page to control them.',
+    'title' => 'Easy AdSense provides a very easy way to generate revenue from your blog using Google AdSense. It can insert ads into posts and sidebar, and add a Google Search box. With its full set of features, Easy AdSense is perhaps the first plugin to give you a complete solution for everything AdSense-related.') ;
+
+$myPlugins['adsense-now'] =
+  array('value' => 'AdSense Now!',
+    'support' => 'UE28AQZUTTX8Q',
+    'price' => '3.95' ,
+    'desc' => 'gets you started with Google AdSense. No mess, no fuss.',
+    'pro' => 'The Lite version of AdSense Now! is fully functional. However, it has an ad slot sharing mechanism built in whereby you can donate about 5% of your ad spaces to the author of the plugin. It may also put a small powered-by kind of link below the first ad block on your pages. The Pro version has these options turned off and gives you options on the admin page to control them.',
+    'title' => 'AdSense Now! is the simplest possible way to generate revenue from your blog using Google AdSense. Aiming at simplicity, AdSense Now! does only one thing: it puts your AdSense code in up to three spots in your posts and pages (both existing ones and those yet to be written).') ;
+
+$myPlugins['easy-ads'] =
+  array('value' => 'Easy Ads',
+    'support' => 'YBB5HXSJ97C7E',
+    'price' => '8.95' ,
+    'desc' => 'showcases AdSense and its alternatives on your blog: Chitika, BidVertiser, Clicksor etc.',
+    'pro' => 'The Lite version of Easy Ads is fully functional. It has an ad slot sharing mechanism built in whereby you can donate about 5% of your ad spaces to the author of the plugin. It also puts a small powered-by kind of link below the first ad block on your pages. The Pro version gives you options on the admin page to turn off these two features.',
+    'title' => 'Easy Ads provides a unified and intuitive interface to manage multiple ad providers on your blog. Currently supported are Chitika, BidVertiser, Clicksor, and, of course, AdSense. The list of ad providers will be expanded later on at your request. AdSense dumped you? Do not be heartbroken; there are other fish in the sea. You may find happiness with <a href="http://www.clicksor.com/pub/index.php?ref=105268" title="Careful, do not double-date with AdSense and Clicksor, they get very jealous of each other!">Clicksor</a>, <a href="http://www.bidvertiser.com/bdv/bidvertiser/bdv_ref_publisher.dbm?Ref_Option=pub&amp;Ref_PID=229404" title="Another fine ad provider">BidVertiser</a> or <a href="http://chitika.com/publishers.php?refid=manojt" title="Compatible with AdSense">Chitika</a>. Use <a href="http://wordpress.org/extend/plugins/easy-ads/" title="A new plugin to handle AdSense and its alternatives">Easy Ads</a>, and you may get lucky!') ;
+
+$myPlugins['theme-tweaker'] =
+  array('value' => 'Theme Tweaker',
+    'support' => 'UKPDMR89Z22DN',
+    'price' => '3.95' ,
+    'desc' => 'lets you modify the colors in your theme with no CSS/PHP editing.',
+    'pro' => 'Note that Theme Tweaker may not work with some themes. Please verify its suitability using the Lite version first. The Lite version of the plugin is fully functional. It puts a small powered-by kind of link at the bottom of your pages though. The Pro version gives you an option on the admin page to turn off the back link.',
+    'title' => 'Theme Tweaker displays the existing colors from your current theme, and gives you a color picker to replace them. It also lets you change them in bulk, like invert all colors, use grey scale etc.') ;
+
+$myPlugins['easy-latex'] =
+  array('value' => 'Easy WP LaTeX',
+    'support' => 'UFFMGT9QHJY2N',
+    'price' => '2.95' ,
+    'desc' => 'provides a very easy way to display math and equations in your posts.',
+    'pro' => 'The Lite version of the plugin is fully functional, although it links the first two equations generated back to the plugin information page and the Unreal Blog by the author. The Pro version gives you an option to suppress these links.',
+    'title' => 'Easy WP LaTeX provides a very easy way to display equations or mathematical formulas (typed in as TeX or LaTeX code) in your posts. It translates LaTeX formulas like this [math](a+b)^2 = a^2 + b^2 + 2ab[/math] into this:<br/>&nbsp;&nbsp;&nbsp;&nbsp;<img src="http://l.wordpress.com/latex.php?latex=(a%2bb)^2%20=%20a^2%20%2b%20b^2%20%2b%202ab&amp;bg=E2E7FF&amp;s=1" style="vertical-align:-70%;" alt="(a+b)^2 = a^2 + b^2 + 2ab" />') ;
+
+$myPlugins['easy-translator'] =
+  array('value' => 'Easy Translator',
+    'support' => '48XZQ7LRDAV28',
+    'price' => '1.95' ,
+    'desc' => 'is a plugin translation tool for authors and translators. (Not a blog page translator!)',
+    'pro' => 'There is hardly any difference between the Lite and Pro versions of this plugin. What you get for the price of the Pro version is only my gratitude.',
+    'title' => 'Easy Translator is a plugin to translate other plugins. It picks up translatable strings (in _[_e]() functions) and presents them and their existing translations (from the MO object of the current text-domain, if loaded) in a user editable form. It can generate a valid PO file that can be emailed to the plugin author directly from the its window, streamlining your work.') ;
+
+?>
